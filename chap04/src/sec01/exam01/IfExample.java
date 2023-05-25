e4afb432b9e17d15074875ceecab007314d2c83b
kslee0421 <kslee0421@gmail.com>
2023-05-25 18:19:22 +0900

    singleton

--- chap04/src/sec01/exam01/IfExample.java
+++ chap04/src/sec01/exam01/IfExample.java
@@ -10,9 +10,9 @@ public class IfExample {
 			System.out.println("등급은 A 입니다");
 		}
 		
-		if(score<90) 
+		if(score<90) { 
 			System.out.println("점수가 90보다 작습니다");
-		
+		}
 		System.out.println("등급은 B 입니다.");
 		
 		//중괄호를 안치면!!!??!?!
--- chap06/src/sec04/exam06/Calculator.java
+++ chap06/src/sec04/exam06/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
 		return width * width;
 	}
 	//직사각형의 넓이
-	double areaRectangle(double width, double width ) {
-		return width * width;
+	double areaRectangle(double width, double height ) {
+		return width * height;
 }
 }
